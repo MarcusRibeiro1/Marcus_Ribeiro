6103bd6de3f70bc99c08ee8b214c0974f0805854
joaopaulo159 <84049992+joaopaulo159@users.noreply.github.com>
2022-03-09 19:55:08 -0300

    Update minhas respostas via web.

---
+++ Git_e_Git_JOAO_BARREIRA/Historia_Github_JOAO_BARREIRA.py
@@ -0,0 +1,9 @@
+Quem inventou o sistema de controle de versão Git e por quê?
+"Linus Torvalds
+O Git é um projeto de código aberto maduro e com manutenção ativa desenvolvido em 2005 por Linus Torvalds, o famoso criador do kernel do sistema operacional Linux."
+Quem atualmente é o dono do Github e por quê?
+"Microsoft
+A aquisição do GitHub pela Microsoft por US$ 7,5 bilhões pegou muita gente de surpresa."
+
+Existe outra maneira, além do terminal, de trabalhar com o Github?
+Sim,é possível via web e no windowns pelo app do desktop.
